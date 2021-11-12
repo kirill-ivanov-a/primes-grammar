3457b78be7fcb7c4857197dde56558453606fed9
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2021-11-12 18:14:27 +0300

    Restructed machines package, added TM to Unrestricted Grammar converter

---
+++ src/machines/state.py
@@ -0,0 +1,12 @@
+__all__ = ["State"]
+
+
+class State:
+    def __init__(self, value: str):
+        self.value = value
+
+    def __eq__(self, other):
+        if not isinstance(other, State):
+            return False
+        else:
+            return self.value == other.value
--- src/machines/turing_machine.py
+++ src/machines/turing_machine.py
@@ -1,53 +1,9 @@
 from typing import Set, Dict
 
+from src.machines.state import State
+from src.machines.transition import TransitionContext, Transition
+
 __all__ = ["TuringMachine"]
-
-
-class Direction:
-    LEFT = -1
-    STAY = 0
-    RIGHT = 1
-
-
-class State:
-    def __init__(self, value: str):
-        self.value = value
-
-    def __eq__(self, other):
-        if not isinstance(other, State):
-            return False
-        else:
-            return self.value == other.value
-
-
-class TransitionContext:
-    def __init__(self, state: State, tape_symbol: str):
-        self.state = state
-        self.tape_sym = tape_symbol
-
-    @classmethod
-    def from_string(cls, transition_context_string):
-        from_state, tape_symbol = transition_context_string.split(",")
-        return TransitionContext(state=State(from_state), tape_symbol=tape_symbol)
-
-
-class Transition:
-    def __init__(self, context_to: TransitionContext, direction: Direction):
-        self.context_to = context_to
-        self.direction = direction
-
-    @classmethod
-    def from_string(cls, transition_string):
-        symbol_to_direction = {
-            "<": Direction.LEFT,
-            "-": Direction.STAY,
-            ">": Direction.RIGHT,
-        }
-        to_state, tape_symbol, direction = transition_string.split(",")
-        return Transition(
-            context_to=TransitionContext(State(to_state), tape_symbol=tape_symbol),
-            direction=symbol_to_direction[direction],
-        )
 
 
 class TuringMachine:
