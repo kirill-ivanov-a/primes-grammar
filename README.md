ac12b9219fe624c91ba811bed7ec1220d023e08e
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2021-11-23 22:42:45 +0300

    Added docstring to config.py

--- README.md
+++ README.md
@@ -7,18 +7,25 @@ Yet another machine to grammar converter. The following transformations are avai
 
 Prime grammars are located [here](https://github.com/kirill-ivanov-a/primes-grammar/tree/main/resources).
 
-## Getting Started
+## Requirements
 
-1. ...
-2. ...
-3. ...
+Python >=3.8. No external libraries were used.
 
 ## Usage
 
 ```bash
-usage: python -m  ...
+usage: python -m src <args>
+
+arguments:
+  -n, --number
+        "Decimal number for primality check"
 
 optional arguments:
   -h, --help
+        "Show help message and exit"
+  -g, --grammar,  default=t0
+        "Choose grammar (t0 | t1) for primality check"
+  -d, --derivation,  default=1
+        "If 1 is passed, show derivation of the number in given grammar"
 ```
 ## Examples
--- src/config.py
+++ src/config.py
@@ -3,14 +3,24 @@ import logging
 
 __all__ = ["Config"]
 
-
+# Config for logger. Is used to output messages in checker.py
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 
 class Config:
+    """
+    Config for the primality checker function
+    """
+
+    # Path to the root of current project
     ROOT = pathlib.Path(__file__).parent.parent
+    # Path to the file where to generate and use T0 grammar
     T0_GRAMMAR_PATH = ROOT / "resources" / "unrestricted_grammar_primes.txt"
+    # Path to the file where to generate and use T1 grammar
     T1_GRAMMAR_PATH = ROOT / "resources" / "csg_primes.txt"
+    # Path to the initial configuration of Turing Machine
     TURING_MACHINE_PATH = ROOT / "resources" / "turing_machine.txt"
+    # Path to the initial configuration of LBA
     LBA_PATH = ROOT / "resources" / "lba.txt"
-    GENERATE_GRAMMAR = True
+    # If True, regenerate T0 / T1 grammar on every run
+    GENERATE_GRAMMAR = False
